f0fe9aa594f1c5eccbdcc9d56c138b5db339419e
Montras59 <salutmontras@gmail.com>
2017-12-02 20:15:07 +0100

    Remove Accès BDD

--- src/fr/doctorwho/service/Database.java
+++ src/fr/doctorwho/service/Database.java
@@ -19,7 +19,6 @@ public class Database extends SQLConnection implements Runnable{
 
 	/** Reconnect to the DB every hour*/
 	public Database() {
-		super("jdbc:mysql://", "localhost", "doctorwho", "oons", "BdjrUjd1fd");
 		
 		 // 20 tick * 60 Seconde * 60 minute
 		task = Bukkit.getScheduler().runTaskTimer(API.getInstance(), this, 0, 20 * 60 * 60);
